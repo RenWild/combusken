d9c35de77955290746c9946814e30b5c1e016dca
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2019-12-15 13:52:53 +0100

    Another fomrula

--- engine/search.go
+++ engine/search.go
@@ -746,7 +746,7 @@ var lmrReductions [64][64]int
 func init() {
 	for depth := 1; depth < 64; depth++ {
 		for movesPlayed := 1; movesPlayed < 64; movesPlayed++ {
-			lmrReductions[depth][movesPlayed] = int(0.75 + math.Log(float64(depth))*math.Log(float64(movesPlayed))/2.25)
+			lmrReductions[depth][movesPlayed] = int(0.9 + math.Log(float64(depth))*math.Log(float64(movesPlayed))/2.45)
 		}
 	}
 }
